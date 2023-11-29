812c751a42373b6c21ed05548e3d372a632fd559
Gerrit Grunwald <han.solo@mac.com>
2023-11-29 10:33:25 +0100

    cosmetics

--- start-manual-crac.sh
+++ start-manual-crac.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export CRAC_CRIU_OPTS=--compress
+# Enable checkpoint compression (will reduce restore time a bit)
+#export CRAC_CRIU_OPTS=--compress
 
 START_TIME=$(($(date +'%s * 1000 + %-N / 1000000')))
 java -DSTART_TIME=$START_TIME -XX:CRaCCheckpointTo=./tmp_manual_checkpoint -jar build/libs/spring-petclinic-3.2.0.jar
