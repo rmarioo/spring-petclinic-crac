0738965872ad86faac61e55ada905c9945d216c8
Gerrit Grunwald <han.solo@mac.com>
2023-11-29 10:20:29 +0100

    cosmetics

--- restore-auto-crac.sh
+++ restore-auto-crac.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 START_TIME=$(($(date +'%s * 1000 + %-N / 1000000')))
-java -DSTART_TIME=$START_TIME -XX:CRaCRestoreFrom=./tmp_checkpoint
+java -DSTART_TIME=$START_TIME -XX:CRaCRestoreFrom=./tmp_auto_checkpoint
